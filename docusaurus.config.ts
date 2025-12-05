e72129be936d8e86b5dfe735d7ac5cbf86d35022
Syed Mujtaba Abbas Zaidi <166711387+syed-mujtaba-stack@users.noreply.github.com>
2025-12-05 16:22:39 +0500

    Update baseUrl in docusaurus.config.ts
    Changed base URL for deployment configuration.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://syed-mujtaba-stack.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Physical-AI-Humanoid-Robotics/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   organizationName: 'syed-mujtaba-stack', // GitHub username
